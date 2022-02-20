d119ee3292171c7ef64fea48922330f2c124a455
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-20 15:13:14 +0000

    shop constructor created

--- Code/src/main/java/Shop.java
+++ Code/src/main/java/Shop.java
@@ -1,2 +1,12 @@
+import behaviors.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<>();
+    }
 }
