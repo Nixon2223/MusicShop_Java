e272a78acc6b586cbc06ad4bb4d23a22cb4e2e0d
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-21 01:36:00 +0000

    getters and setters created for instruments

--- Code/src/main/java/instruments/Instrument.java
+++ Code/src/main/java/instruments/Instrument.java
@@ -11,6 +11,46 @@ public abstract class Instrument implements ISell, IPlay {
     protected double buyPrice;
     protected double sellPrice;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
+
+    public void setSellPrice(double sellPrice) {
+        this.sellPrice = sellPrice;
+    }
+
     public Instrument(String type, String colour, String material, double buyPrice, double sellPrice) {
         this.type = type;
         this.colour = colour;
---
+++ Code/src/test/java/instruments/GuitarTest.java
@@ -0,0 +1,19 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar("String","red","ash",50.00,75.00, 6);
+    }
+
+    @Test
+    public void hasType(){
+        assert
+    }
+}
